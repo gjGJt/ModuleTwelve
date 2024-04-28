e0da19a83a73b8228e09d20f91a096d944cea7dc
gjGJt <157390876+gjGJt@users.noreply.github.com>
2024-04-27 23:01:26 -0500

    Update Moduletwelvepointone.cpp

--- Moduletwelvepointone.cpp
+++ Moduletwelvepointone.cpp
@@ -1,64 +1,83 @@
- #include <iostream>
+  #include <iostream>
 using namespace std;
-void merge(int *,int, int , int );
-void merge_sort(int *arr, int low, int high)
+
+// Functions  ( I used the names in the outline)
+void merge(int arr[], int left, int mid, int right);
+void merge_sort(int arr[], int left, int right);
+
+ 
+void merge_sort(int arr[], int left, int right)
 {
     int mid;
-    if (low < high){
-        //divide the array at mid and sort independently using merge sort
-        mid=(low+high)/2;
-        merge_sort(arr,low,mid);
-        merge_sort(arr,mid+1,high);
-        //merge or conquer sorted arrays
-        merge(arr,low,high,mid);
+    if (left < right) {
+        // Dividing array at mid and then sort
+        mid = (left + right) / 2;
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
+        // Merging sorted arrays
+        merge(arr, left, mid, right);
     }
 }
-// Merge sort
-void merge(int *arr, int low, int high, int mid)
+
+// Merge function
+void merge(int arr[], int left, int mid, int right)
 {
     int i, j, k, c[50];
-    i = low;
-    k = low;
+    i = left;
+    k = left;
     j = mid + 1;
-    while (i <= mid && j <= high) {
+    // using a temporary array to merge the two halves
+    while (i <= mid && j <= right) {
         if (arr[i] < arr[j]) {
             c[k] = arr[i];
             k++;
             i++;
         }
-        else  {
+        else {
             c[k] = arr[j];
             k++;
             j++;
         }
     }
+    // Copy remaining elements of left
     while (i <= mid) {
         c[k] = arr[i];
         k++;
         i++;
     }
-    while (j <= high) {
+    // Copy remaining elements of right
+    while (j <= right) {
         c[k] = arr[j];
         k++;
         j++;
     }
-    for (i = low; i < k; i++)  {
+    // Copying the sorted elements to the original array
+    for (i = left; i < k; i++) {
         arr[i] = c[i];
     }
 }
-// read input array and call mergesort
+
+// this is the main function
 int main()
 {
     int myarray[30], num;
-    cout<<"Enter number of elements to be sorted:";
-    cin>>num;
-    cout<<"Enter "<<num<<" elements to be sorted:";
-    for (int i = 0; i < num; i++) { cin>>myarray[i];
+    // Inputted number of elements to be sorted
+    cout << "Enter number of elements to be sorted: ";
+    cin >> num;
+    // Inputted elements to be sorted
+    cout << "Enter " << num << " elements to be sorted: ";
+    for (int i = 0; i < num; i++) {
+        cin >> myarray[i];
     }
-    merge_sort(myarray, 0, num-1);
-    cout<<"Sorted array\n";
-    for (int i = 0; i < num; i++)
-    {
-        cout<<myarray[i]<<"\t";
+    // Calling the merge_sort function for sorting the array
+    merge_sort(myarray, 0, num - 1);
+    // sorted array outputted
+    cout << "Sorted array:\n";
+    for (int i = 0; i < num; i++) {
+        cout << myarray[i] << "\t";
     }
+    cout << endl;
+    return 0;
 }
+
+
